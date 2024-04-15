8ce9be91f389d3843335e699770d53d4b14c6b87
Amber <60221403+amber871023@users.noreply.github.com>
2024-04-15 14:28:05 +1000

    add Header and Footer components, assets folder containing images files

---
+++ src/components/Footer.js
@@ -0,0 +1,72 @@
+import { Box, Container, Stack, Image, Button, Text, HStack, VStack, Spacer, Heading, IconButton, Link } from '@chakra-ui/react'
+import logo from '../assets/logo_white.png';
+import { Link as RouterLink } from 'react-router-dom';
+import { FaEnvelope, FaLinkedin, FaInstagram, FaFacebookSquare } from 'react-icons/fa';
+
+const MenuItems = ['About', 'Resume', 'Portfolio'];
+export default function Footer() {
+  return (
+    <Box bg={'primary.500'} color={'white'}>
+      <Container
+        maxW={'6xl'} py={4} spacing={4} justify={'center'}
+        align={'space-between'}>
+        <Stack direction={{ base: 'column', md: 'row' }}>
+          {/* MenuItems */}
+          <Stack pl={3} spacing={{ base: '3', md: '1' }} direction={{ base: 'row', md: 'column' }} justify={'center'} >
+            <Button
+              as={RouterLink} to={'/'} colorScheme='white' variant='link' textStyle={'h4'}>Home</Button>
+            {MenuItems.map((item) => (
+              <Button
+                as={RouterLink} to={`/${item}`} key={item} colorScheme='white' variant='link' mt={1} textStyle={'h4'}>{item}</Button>
+            ))}
+          </Stack>
+          <Spacer />
+          {/* Contact info only display in base mode  */}
+          <Box display={{ base: '', md: 'none' }} align={'center'} mt={2}>
+            <Heading size={'sm'} textAlign={{ base: 'center', md: 'start' }} mb={1}>Contact</Heading>
+            <Link href='mailto:n11422807@qut.edu.au' isExternal>
+              <IconButton px={0} icon={<FaEnvelope />} colorScheme='primary.500' aria-label="Email" fontSize='30px' />
+            </Link>
+            <Link href='www.linkedin.com/in/amber-cheng-202396227' isExternal>
+              <IconButton icon={<FaLinkedin />} aria-label="LinkedIn" colorScheme='primary.500' fontSize='30px' />
+            </Link>
+            <Link href="https://www.facebook.com/amberCYT" isExternal>
+              <IconButton icon={<FaFacebookSquare />} aria-label="Facebook" colorScheme='primary.500' fontSize='30px' />
+            </Link>
+            <Link href="https://www.instagram.com/amber_cyttt/" isExternal>
+              <IconButton icon={<FaInstagram />} aria-label="Instagram" colorScheme='primary.500' fontSize='30px' />
+            </Link>
+          </Box>
+          {/* logo and copyright */}
+          <VStack>
+            <Box as={RouterLink} to={'/'}>
+              <Image src={logo} alt='yi ting' w="150px" h="60px" />
+            </Box>
+            <Text borderTopWidth={1} borderStyle={'solid'} borderColor={'white'}>© 2024 YiTing Cheng. All rights reserved</Text>
+          </VStack>
+          <Spacer />
+          {/* Contact info */}
+          <Box display={{ base: 'none', md: 'block' }} align={'left'} mt={2}>
+            <Heading size={'sm'} textAlign={{ base: 'center', md: 'start' }} mb={1}>Contact</Heading>
+            <HStack >
+              <Link href='mailto:n11422807@qut.edu.au' isExternal>
+                <IconButton px={0} icon={<FaEnvelope />} colorScheme='primary.500' aria-label="Email" fontSize='26px' />
+                <span>n11422807@qut.edu.au</span>
+              </Link>
+            </HStack>
+
+            <Link href='www.linkedin.com/in/amber-cheng-202396227' isExternal>
+              <IconButton icon={<FaLinkedin />} aria-label="LinkedIn" colorScheme='primary.500' fontSize='30px' />
+            </Link>
+            <Link href="https://www.facebook.com/amberCYT" isExternal>
+              <IconButton icon={<FaFacebookSquare />} aria-label="Facebook" colorScheme='primary.500' fontSize='30px' />
+            </Link>
+            <Link href="https://www.instagram.com/amber_cyttt/" isExternal>
+              <IconButton icon={<FaInstagram />} aria-label="Instagram" colorScheme='primary.500' fontSize='30px' />
+            </Link>
+          </Box>
+        </Stack>
+      </Container>
+    </Box >
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Container, Flex, Box, Button, Image, Spacer, Stack } from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import logo from '../assets/logo.png';
+
+const MenuItems = ['About', 'Resume', 'Portfolio'];
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Box as={'nav'} bg='white' boxShadow='base' position='sticky' top='0' left='0' right='0' zIndex='100' width='100%' >
+      <Container maxW={'7xl'}>
+        <Flex p='2' position='relative'>
+          {/* Logo */}
+          <Box as={NavLink} to={'/'}>
+            <Image src={logo} alt='yi ting' w={{ base: '120px', md: '150px' }} h={{ base: '50px', md: '55px' }} />
+          </Box>
+          <Spacer />
+          {/* Menu */}
+          <Box display={{ base: 'none', md: 'flex' }}>
+            <Button
+              as={NavLink} to={`/`} style={({ isActive }) => ({ color: isActive ? "brown" : "black" })} variant='link' mr={5} textStyle={'h3'} onClick={closeMenu}>Home</Button>
+            {MenuItems.map((item) => (
+              <Button
+                as={NavLink} to={`/${item}`} key={item} style={({ isActive }) => ({ color: isActive ? "brown" : "black" })} variant='link' mr={5} textStyle={'h3'}>{item}</Button>
+            ))}
+          </Box>
+          {/* toggleMenu-hamburger */}
+          <Box display={{ base: 'flex', md: 'none' }}>
+            <Button onClick={toggleMenu} colorScheme='white' variant='link'>
+              {isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            </Button>
+          </Box>
+          {isOpen && (
+            //display it in md size and hidden in base size
+            <Stack display={{ base: 'flex', md: 'none' }} position='fixed' top='63px' left='0' bg='primary.500' p='2' mt='1' zIndex='100' width='100%'>
+              <Flex direction='column'>
+                <Button
+                  as={NavLink} to={'/'} colorScheme='white' color={'white'} variant='link' mb='1' onClick={closeMenu}>Home</Button >
+                {
+                  MenuItems.map((item) => (
+                    <Button
+                      as={NavLink} to={`/${item}`} key={item} colorScheme='white' color={'white'} variant='link' mb='1' onClick={closeMenu}>{item}</Button>
+                  ))
+                }
+              </Flex>
+            </Stack>
+          )}
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+export default Header;
